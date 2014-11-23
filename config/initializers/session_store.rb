b4b79540d8681f3f5892d8988c30af3f539a9223
Masa <cmasad743@gmail.com>
2014-11-23 13:24:07 +0900

    fix gitignore

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-<<<<<<< HEAD
-Rails.application.config.session_store :cookie_store, key: '_aoba_session'
-=======
 Rails.application.config.session_store :cookie_store, key: '_Aoba_session'
->>>>>>> dfd3e4795418ab04bf8df8689048e2dc9db2f6ba
